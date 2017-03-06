0488fb7834e072745842e12d20a70b241bd54a22
Justin Samuels <blizm87@gmail.com>
2017-03-06 00:49:11 -0800

    Completed template for portfolio

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const morgan = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
+// CONFIG
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'main', layoutsDir: __dirname + '/views/layouts/'}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
 app.use(morgan('dev'));
+
+// ROUTES
+app.use('/', require('./routes/index'));
 
 const port = 3000;
 
