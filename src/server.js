63e85ad10caa03d05e91a8df5b5dfeebc0abf727
Justin Samuels <blizm87@gmail.com>
2017-03-06 01:54:54 -0800

    Added a functional stylesheet

--- src/server.js
+++ src/server.js
@@ -6,11 +6,12 @@ const hbs = require('express-handlebars');
 const app = express();
 
 // CONFIG
-app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'main', layoutsDir: __dirname + '/views/layouts/'}));
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'main', layoutsDir: path.join(__dirname, '/views/layouts/')}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
-app.use(morgan('dev'));
 
 // ROUTES
 app.use('/', require('./routes/index'));
