0dfef89acde381e9be7595548f45c69a3f290c47
Justin Samuels <blizm87@gmail.com>
2017-03-09 13:19:33 -0800

    added a body-parser to handle email data

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const path = require('path');
 const hbs = require('express-handlebars');
 
@@ -7,6 +8,8 @@ const app = express();
 
 // CONFIG
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'main', layoutsDir: path.join(__dirname, '/views/layouts/')}));
