b4172b750dd0ba5d74d1c337042ad22a371c803f
Justin Samuels <blizm87@gmail.com>
2022-01-29 14:26:49 -0800

    restored original portfolio to active display

--- .originalPortfolio/README.md
+++
@@ -1,2 +0,0 @@
-# My-Portfoliio
-A MEAN stack website holding a portfolio of my best projects
--- server.js
+++ server.js
@@ -1,11 +1,28 @@
-const
-  express = require('express'),
-  serveStatic = require('serve-static'),
-  history = require('connect-history-api-fallback'),
-  port = process.env.PORT || 5000
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const path = require('path')
+const hbs = require('express-handlebars')
 
 const app = express()
 
-app.use(history())
-app.use(serveStatic(__dirname + '/dist/spa'))
-app.listen(port)
+// CONFIG
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, './public')))
+
+app.engine('hbs', hbs({ extname: 'hbs', defaultLayout: 'main', layoutsDir: path.join(__dirname, './src/views/layouts/') }))
+app.set('views', path.join(__dirname, 'src/views'))
+app.set('view engine', 'hbs')
+
+// ROUTES
+app.use('/', require('./src/routes/index'))
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log('I am listening on port 3000')
+})
+
+module.exports = app
